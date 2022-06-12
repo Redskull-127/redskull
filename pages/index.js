d608f38a7a43bb27413c971ee62c641c9f7a1018
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-12 21:00:26 +0530

    Meta Content

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,11 @@ export default function Home() {
         <Head>
           <title>REDSKULL</title>
           <meta name="description" content="By Meer Tarbani" />
-          <link rel="icon" href="cursor.png" />
+          <link rel="shortcut icon" href="cursor.png" />
+          <meta
+            name="keywords"
+            content="Meer Tarbani, Meer, Tarbani, Portfolio, Website, meer tarbani, Redskull, redskull, Redskull.me, redskull.me, redskull me, Redskull me, redskull meer tarbani, redskull,meer tarbani acid, meer tarbani as a fraction, meer tarbani ba, meer tarbani bali, meer tarbani bangla, meer tarbani bangla lyrics, meer tarbani battery, meer tarbani bank, meer tarbani blood pressure, meer tarbani chords, meer tarbani com, meer tarbani code, meer tarbani center, meer tarbani de, meer tarbani delhi, meer tarbani dei, meer tarbani dit, meer tarbani definition, meer tarbani download, meer tarbani disease, meer tarbani english translation, meer tarbani english lyrics, meer tarbani english, meer tarbani english pdf, meer tarbani english subtitles, meer tarbani englisch, meer tarbani example, meer tarbani film, meer tarbani facebook, meer tarbani full movie, meer tarbani fakaza, meer tarbani font, meer tarbani file, meer tarbani for sale,  eer tarbani gif, meer tarbani google translate,  meer tarbani google scholar, meer tarbani google, meer tarbani google drive, meer tarbani games, meer tarbani germany,"
+          />
           <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
             rel="stylesheet"
